1f3bb82f1d6964021d198d6a603b352cb7f69ce5
Julian <Paschetag>
2020-10-12 17:24:47 +0200

    Initial commit

---
+++ http_request.php
@@ -0,0 +1,58 @@
+<?php
+include ("postExecute.php");
+$mysqli = new mysqli("localhost","root","","accounts");
+
+$mode = $_GET["MODE"];
+$sql = "";
+
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    return;
+}else {
+    switch ($mode) {
+
+        //Registrierung
+        case 0:
+            $sql = "INSERT INTO USER (VORNAME,NACHNAME,PASSWORD,EMAIL,USER_KENNUNG) VALUES ('".$_GET["VORNAME"]."','".$_GET["NACHNAME"]."','".$_GET["PASSWORT"]."','".$_GET["MAIL"]."','".uniqid()."')";
+            break;
+        //Login
+        case 1:
+            $sql = "SELECT * FROM USER WHERE EMAIL ='".$_GET["MAIL"]."' AND PASSWORD='".$_GET["PASSWORT"]."'";
+            break;
+    }
+}
+// Confirm there are results
+$arrReturn = Array();
+$rows = 0;
+
+if ($mysqli->multi_query($sql))
+{
+    do {
+        /* store first result set */
+        if ($result = $mysqli->store_result()) {
+
+            while ($row = $result->fetch_assoc()) {
+                array_push($arrReturn,$row);
+            }
+            $rows .= $result->num_rows;
+            $result->free();
+        }
+
+    } while ($mysqli->next_result());
+
+    //Auch wenn kein ergebnis zurückgegeben wird den error zurückgeben
+    if($rows > 0){
+        $arrReturn = POST_EECUTE::appendSuccessData($arrReturn,$mode);
+    }else{
+        $arrReturn = POST_EECUTE::appendErrorData($arrReturn,$mode);
+    }
+}else{
+    $arrReturn = POST_EECUTE::appendErrorData($arrReturn,$mode);
+    $arrReturn["ERROR_CODE"] = $mysqli->errno;
+
+}
+
+echo json_encode($arrReturn);
+
---
+++ postExecute.php
@@ -0,0 +1,36 @@
+<?php
+
+class POST_EECUTE{
+
+    public static function appendSuccessData($arrReturn,$mode){
+
+        switch ($mode){
+            case 0:
+                $arrReturn["REGISTER_SUCCESS"] = true;
+                $arrReturn["MAIL"] = $_GET["MAIL"];
+                $arrReturn["PASSWORT"] = $_GET["PASSWORT"];
+                break;
+            case 1:
+                $arrReturn["LOGIN_SUCCESS"] = true;
+                break;
+        }
+
+        return $arrReturn;
+    }
+
+    public static function appendErrorData($arrReturn,$mode){
+
+        switch ($mode){
+            case 0:
+                $arrReturn["REGISTER_SUCCESS"] = false;
+                break;
+            case 1:
+                $arrReturn["LOGIN_SUCCESS"] = false;
+                break;
+        }
+
+
+        return $arrReturn;
+    }
+
+}
